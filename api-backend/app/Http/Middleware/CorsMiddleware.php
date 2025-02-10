59fdb79cc1ca8a1e69e6e6c3d300a2a765d67b25
hridi1912 <hriditaalam1@gmail.com>
2025-02-10 12:03:27 +0600

    fixed route problem

--- api-backend/app/Http/Middleware/CorsMiddleware.php
+++ api-backend/app/Http/Middleware/CorsMiddleware.php
@@ -14,7 +14,7 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:5174'); // Allow Vite frontend
+        $response->headers->set('Access-Control-Allow-Origin', $request->headers->get('Origin') ?? '*'); // Allow Vite frontend
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
