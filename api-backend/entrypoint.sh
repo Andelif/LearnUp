caf1a4f67c0dd9015a7e9b5569ca53c9cf04615e
Andelif Hossain <andelif.cse.20220104052@aust.edu>
2025-03-09 03:04:57 +0600

    Last try

--- api-backend/entrypoint.sh
+++ api-backend/entrypoint.sh
@@ -3,7 +3,8 @@
 # Wait for MySQL to be available
 echo "Waiting for MySQL..."
 until mysql -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" "$DB_DATABASE" -e "SELECT 1" &>/dev/null; do
-  sleep 2
+  echo "Waiting for MySQL to be ready..."
+  sleep 10
 done
 
 # Install Composer dependencies
