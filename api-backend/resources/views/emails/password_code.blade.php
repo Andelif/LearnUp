6609fadc1dfd0534448a1e093353add23947505c
Andelif <andelif.cse.20220104052@aust.edu>
2025-09-10 03:11:28 +0600

    Password Reset functionality added

---
+++ api-backend/resources/views/emails/password_code.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <body style="font-family:Arial, Helvetica, sans-serif; color:#222;">
+    <p>Use this code to reset your password:</p>
+    <h2 style="letter-spacing:4px; font-size:28px; margin:12px 0;">{{ $code }}</h2>
+    <p>This code expires in 10 minutes.</p>
+    <p>If you didn’t request a reset, you can ignore this email.</p>
+  </body>
+</html>
